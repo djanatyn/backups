b03bef6f32b5f58571351c2c5965bfeccc21004f
Jonathan Strickland <djanatyn@gmail.com>
2022-11-11 14:00:27 -0500

    draft next steps

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use clap::Parser;
 use std::io;
 
 mod args;
@@ -6,21 +5,63 @@ mod repository;
 use crate::args::{Action, Args, Init};
 use crate::repository::Repository;
 
+pub struct SystemdCreds {}
+impl SystemdCreds {
+    pub fn new(repo: &Repository) -> SystemdCreds {
+        todo!();
+    }
+}
+
+pub struct Bucket {}
+impl Bucket {
+    pub fn new(repo: &Repository) -> Bucket {
+        todo!();
+    }
+
+    pub fn sync(&self) {
+        todo!();
+    }
+}
+
+pub struct Manifest {
+    pub repo: Repository,
+    pub bucket: Bucket,
+    pub creds: SystemdCreds,
+}
+
+impl Manifest {
+    pub fn save(&self) {
+        todo!();
+    }
+}
+
 /// Initialize a borg repository and run the first backup!
 fn init(args: Init) -> io::Result<()> {
+    // create repository + run first backup
     let repo = Repository::new(args.name, args.path);
     repo.backup("initial backup");
 
-    // TODO: store borg key in password manager,
-    // TODO: generate systemd-creds,
-    // TODO: create backblaze-b2 bucket,
-    // TODO: upload borg repository to b2 bucket
+    // sync with backblaze
+    let bucket = Bucket::new(&repo);
+    bucket.sync();
 
-    todo!();
+    // add credentials to systemd
+    let creds = SystemdCreds::new(&repo);
+
+    // record what happened
+    let manifest = Manifest {
+        repo,
+        bucket,
+        creds,
+    };
+    manifest.save();
+
+    Ok(())
 }
 
 /// Parse arguments and run.
 fn main() -> io::Result<()> {
+    use clap::Parser;
     let subcmd = Args::parse();
 
     match subcmd.action {
